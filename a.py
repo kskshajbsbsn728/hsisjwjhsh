f1f57c6ee4a89cdc30966a9554967caab06f3ee2
kskshajbsbsn728 <112007821+kskshajbsbsn728@users.noreply.github.com>
2022-08-24 23:28:13 +0600

    Create a.py

---
+++ a.py
@@ -0,0 +1,11 @@
+from telegram.ext import *
+
+def wm(u,c):
+    u.message.reply_text(u.message.text)
+    
+
+upd = Updater("5691804740:AAFOJtZTvnpaVYrvtOwfVTWUx1-jSTu0hUM",use_context=true)
+upd.add_handler("start",wm)
+
+upd.start_polling()
+upd.idle()
