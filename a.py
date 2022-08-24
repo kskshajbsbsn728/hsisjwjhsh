d967a645f17a4aa2460f1eddef007d440d3fdd5a
kskshajbsbsn728 <112007821+kskshajbsbsn728@users.noreply.github.com>
2022-08-24 23:33:13 +0600

    Update a.py

--- a.py
+++ a.py
@@ -4,7 +4,7 @@ def wm(u,c):
     u.message.reply_text(u.message.text)
     
 
-upd = Updater("5691804740:AAFOJtZTvnpaVYrvtOwfVTWUx1-jSTu0hUM",use_context=true)
+upd = Updater("5691804740:AAFOJtZTvnpaVYrvtOwfVTWUx1-jSTu0hUM",use_context=True)
 upd.add_handler("start",wm)
 
 upd.start_polling()
